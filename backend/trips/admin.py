8845cda183d382530f6e1ff624b5671c9c9156c8
Muhammad Ahmad Zaheer <ahmedzaheer2004.24@gmail.com>
2026-07-19 21:59:53 +0500

    Added Admin.py

--- backend/trips/admin.py
+++ backend/trips/admin.py
@@ -1,3 +1,39 @@
 from django.contrib import admin
 
-# Register your models here.
+from .models import TripPlan
+
+
+@admin.register(TripPlan)
+class TripPlanAdmin(admin.ModelAdmin):
+    list_display = (
+        "id",
+        "current_location",
+        "pickup_location",
+        "dropoff_location",
+        "current_cycle_used",
+        "distance_miles",
+        "driving_hours",
+        "fuel_stops",
+        "rest_periods",
+        "created_at",
+    )
+
+    list_filter = (
+        "created_at",
+        "fuel_stops",
+        "rest_periods",
+    )
+
+    search_fields = (
+        "current_location",
+        "pickup_location",
+        "dropoff_location",
+    )
+
+    readonly_fields = (
+        "created_at",
+        "request_payload",
+        "response_payload",
+    )
+
+    ordering = ("-created_at",)
